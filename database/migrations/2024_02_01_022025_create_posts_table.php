77d50e1beb70758c56f7fcb2d2cb437317fe15e7
Wahni Adnani <adnaniwahni@gmail.com>
2024-02-01 10:49:38 +0800

    database schema. post and category

--- database/migrations/2024_02_01_022025_create_posts_table.php
+++ database/migrations/2024_02_01_022025_create_posts_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +14,18 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+
+            $table->string('image')->nullable();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+
+            $table->timestamp('published_at')->nullable();
+            $table->boolean('featured')->default(false);
+
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
