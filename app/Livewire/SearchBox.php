1c54c87472df73203ef51be1df078de5d2fa4579
Wahni Adnani <adnaniwahni@gmail.com>
2024-02-01 17:08:13 +0800

    search progres

--- app/Livewire/SearchBox.php
+++ app/Livewire/SearchBox.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class SearchBox extends Component
 {
+    public $search = '';
+
+    public function updatedSearch(){
+        $this->dispatch('search', search: $this->search);
+    }
     public function render()
     {
         return view('livewire.search-box');
